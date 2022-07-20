f21f4ff092125cb98b6c7a4ed71dec42fb80956a
vdorovatas <vdorovatas@hotmail.gr>
2022-07-20 16:36:33 +0300

    first commit

--- src/main/java/com/agileactors/usersproject/UsersProjectApplication.java
+++ src/main/java/com/agileactors/usersproject/UsersProjectApplication.java
@@ -8,6 +8,7 @@ public class UsersProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersProjectApplication.class, args);
+		System.out.println("Beginning...");
 	}
 
 }
